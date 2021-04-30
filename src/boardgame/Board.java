62e082583026ba0cba5e8b7c3f0b06b53baf0c3c
CaioUfc <caiocsd@alu.ufc.br>
2021-04-30 00:53:20 -0300

    154, 155, 156, criando execoes e imcrementando as classes do sistema

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns <1) {
+			throw new BoardException("E nescessario que haja ao menos uma linha e uma coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -15,28 +18,44 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	public Piece piece(int row, int column) {
+		if (!positionExistis(row, column)) {
+			throw new BoardException("Posicao nao existe no tabuleiro");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExistis(position)) {
+			throw new BoardException("Posicao nao inexistente no tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
+	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Ja ha uma pessa na posicao " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
 	
+	public boolean positionExistis(int row, int column) {
+		return row >=0 && row < rows && column >=0 && column < columns;
+	}
+	
+	public boolean positionExistis(Position position) {
+		return positionExistis(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExistis(position)) {
+			throw new BoardException("Posicao nao inexistente no tabuleiro");
+		}
+		return piece(position) != null;
+	}
 }
